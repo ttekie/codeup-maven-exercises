f5a6e17509bb28e2d931cfe23b6c33823b263d8d
tesfalem tekie <teshanna72016@gmail.com>
2022-11-02 11:53:23 -0500

    MavenExercise is done

--- src/main/java/MavenExercise.java
+++ src/main/java/MavenExercise.java
@@ -1,3 +1,20 @@
-public class MavenExercise {
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
+public class MavenExercise {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter something: ");
+        String userInput = scan.nextLine();
+        System.out.println("You entered: " + userInput);
+        if (StringUtils.isNumeric(userInput)) {
+            System.out.println(userInput + " is a number");
+        } else {
+            System.out.println("That is not a number");
+        }
+
+        System.out.println("Flipped case: " + StringUtils.swapCase(userInput));
+        System.out.println("Reverse: " + StringUtils.reverse(userInput));
+    }
 }
